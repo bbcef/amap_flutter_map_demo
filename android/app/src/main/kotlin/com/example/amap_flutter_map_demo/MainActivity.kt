bb85cba382cd8f95bdfff3036f4e30d9b8410bc6
jingbc <961166897@qq.com>
2021-01-28 15:58:12 +0800

    init

---
+++ android/app/src/main/kotlin/com/example/amap_flutter_map_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.amap_flutter_map_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/const_config.dart
@@ -0,0 +1,7 @@
+import 'package:amap_flutter_base/amap_flutter_base.dart';
+
+class ConstConfig {
+  static const AMapApiKey amapApiKeys = AMapApiKey(
+      androidKey: 'a36bf05eb469edbfa9b516ee3e0020b0',
+      iosKey: '4dfdec97b7bf0b8c13e94777103015a9');
+}
